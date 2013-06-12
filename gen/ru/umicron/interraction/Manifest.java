f9bf0cebaf5ba9de2d52fcad64738ed119f690b7
Aleksey Tufyakov <aldap.ru@gmail.com>
2013-06-12 19:19:16 +0400

    Initial commit

---
+++ gen/ru/umicron/interraction/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ru.umicron.interraction;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
